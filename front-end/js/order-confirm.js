d00039367ca6b5b827c681bae78108a75a9cec12
Alexandre VEYLET <alexandre.veylet.pro@gmail.com>
2021-11-08 15:31:01 +0100

    Correction orderConfirm style

--- front-end/js/order-confirm.js
+++ front-end/js/order-confirm.js
@@ -1,9 +1,9 @@
-let getLocStor = JSON.parse(sessionStorage.getItem('contactAfterSendingOrder'));
+let getProfil = JSON.parse(sessionStorage.getItem('contactAfterSendingOrder'));
+let getProduct = JSON.parse(localStorage.getItem('validProduct'));
 
 let sectionConfirm = document.getElementById('sectionConfirm');
-
 let thanks = document.createElement('h1');
-thanks.innerHTML = `${getLocStor.firstName} ${getLocStor.lastName} <br> Nous vous remercions pour votre commande.<br> Voici le numéro de celle ci`;
+thanks.innerHTML = `${getProfil.firstName} ${getProfil.lastName} <br> <br> Nous vous remercions pour votre commande :`;
 
 const getUrl = window.location.search;
 const getOrderId = new URLSearchParams(getUrl);
@@ -13,7 +13,21 @@ let id = document.createElement('h2');
 id.innerHTML = `${orderId}`
 id.style = 'padding-bottom: 5rem;';
 
-document.body.appendChild(thanks);
-document.body.appendChild(id);
-(sectionConfirm).appendChild(thanks);
-(thanks).appendChild(id);
+sectionConfirm.appendChild(thanks);
+thanks.appendChild(id);
+
+for(let i = 0; i < getProduct.length; i++){
+    let divProductPaid = document.createElement('div');
+    let imgProductPaid = document.createElement('img');
+
+    divProductPaid.style.display = 'flex';
+    divProductPaid.style.justifyContent = 'center';
+    imgProductPaid.src = `${getProduct[i].img}`;
+    imgProductPaid.style.width = '50%';
+    imgProductPaid.style.marginBottom = '2rem';
+
+
+    sectionConfirm.appendChild(divProductPaid);
+    divProductPaid.appendChild(imgProductPaid);
+
+}
